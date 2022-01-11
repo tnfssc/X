333c466c05c4b5cb9445f9ac44564e1b909651ca
tnfssc <tnfssc@gmail.com>
2022-01-11 22:26:24 +0530

    Autohide Nav

--- src/components/DirectionsInput/index.tsx
+++ src/components/DirectionsInput/index.tsx
@@ -11,19 +11,30 @@ interface DirectionsInputProps {
 const DirectionsInput: React.FC<DirectionsInputProps> = ({ onGo, onClear }) => {
   const [from, setFrom] = useState("");
   const [to, setTo] = useState("");
+  const [show, setShow] = useState(true);
   const handleGo = () => {
     onGo(from, to);
   };
+  const handleClear = () => {
+    setFrom("");
+    setTo("");
+    onClear && onClear();
+  };
   return (
-    <Block>
-      <AutoCompleteInput onChange={setFrom} placeholder="From" />
-      <AutoCompleteInput onChange={setTo} placeholder="To" />
+    <Block marginTop={!show ? "-146px" : "0px"} overrides={{ Block: { style: { transition: "margin 300ms ease" } } }}>
+      <AutoCompleteInput onChange={setFrom} value={from} placeholder="From" />
+      <AutoCompleteInput onChange={setTo} value={to} placeholder="To" />
       <Block width="100%" display="flex">
-        <Button onClick={onClear} kind="secondary">
+        <Button onClick={handleClear} kind="secondary">
           Clear
         </Button>
         <Button $style={{ flexGrow: 1 }} onClick={handleGo} kind="primary">
           Go
+        </Button>
+      </Block>
+      <Block width="100%" display="flex" justifyContent="center">
+        <Button kind="secondary" shape="pill" size="mini" onClick={() => setShow((p) => !p)}>
+          {show ? "Hide" : "Show"}
         </Button>
       </Block>
     </Block>
