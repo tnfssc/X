cb5e3295a344110956f858b68c16e086376f45de
tnfssc <tnfssc@gmail.com>
2022-01-12 00:48:46 +0530

    PWA working

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { registerSW } from "virtual:pwa-register"; // eslint-disable-line import/no-unresolved
+registerSW();
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,33 @@ dotenvConfig();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ disable: process.env.NODE_ENV === "development" })],
+  plugins: [
+    react(),
+    VitePWA({
+      disable: process.env.NODE_ENV === "development",
+      base: "/",
+      manifest: {
+        name: "X",
+        short_name: "X",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#fff",
+        theme_color: "#fff",
+        icons: [
+          {
+            src: "/favicon.png",
+            sizes: "192x192",
+            type: "image/x-icon",
+          },
+          {
+            src: "/favicon.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+      registerType: "autoUpdate",
+    }),
+  ],
 });
