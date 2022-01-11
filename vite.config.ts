187391225370aa7cd607c06945222299a5c6c5ae
tnfssc <tnfssc@gmail.com>
2022-01-11 22:56:13 +0530

    PWAified

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+import { config as dotenvConfig } from "dotenv";
+dotenvConfig();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({ disable: process.env.NODE_ENV === "development" })],
 });
